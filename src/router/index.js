1f2f4d8b30510bb92267af23c94b03e2dd07d3f9
Cam1409 <eocanaba14@ucvvirtual.edu.pe>
2024-06-17 11:40:20 -0500

    LUNES

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
@@ -29,23 +29,37 @@ const routes = [
   {
     path: '/Menu',
     name: 'MenuView',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Menu/MenuView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Menu/MenuView.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/ReporteGeneral',
     name: 'ReporteGeneralView',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ReporteGeneral/ReporteGeneralView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/ReporteGeneral/ReporteGeneralView.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/ReportePersonalizado',
     name: 'ReportePersonalizadoView',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ReportePersonalizado/ReportePersonalizadoView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/ReportePersonalizado/ReportePersonalizadoView.vue'),
+    meta: { requiresAuth: true }
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+// Guardia de navegación global
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('IdPersonal');
+  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+    // Si la ruta requiere autenticación y el usuario no está autenticado, redirige al login
+    next({ name: 'LoginView' });
+  } else {
+    next(); // De lo contrario, permite la navegación
+  }
+});
+
+export default router;
